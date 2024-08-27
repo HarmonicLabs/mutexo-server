2718c27bfaaa351bf2f0b635cce8fb9631280c89
Raffa Bux <raffaelebussolotto@gmail.com>
2024-08-27 14:05:41 +0200

    updated all the api keys queries

--- src/redis/isFollowingAddr.ts
+++ src/redis/isFollowingAddr.ts
@@ -1,34 +1,33 @@
+import { ADDR_TO_API_SET_PREFIX, API_TO_ADDR_SET_PREFIX, PUBLIC_API_KEY } from "../constants";
 import { AddressStr } from "@harmoniclabs/cardano-ledger-ts";
-import { API_KEY } from "../wsServer/clientProps";
 import { getRedisClient } from "./getRedisClient";
-import { ADDR_TO_API_SET_PREFIX, API_TO_ADDR_SET_PREFIX } from "../constants";
 
-export async function addressIsFollowed( addr: AddressStr ): Promise<boolean>
+export async function addressIsFollowed( ipAddress: AddressStr ): Promise<boolean>
 {
     const redis = await getRedisClient();
-    return await redis.sCard( `${ADDR_TO_API_SET_PREFIX}:${addr}` ) > 0;
+    return await redis.sCard( `${ADDR_TO_API_SET_PREFIX}:${ipAddress}` ) > 0;
 }
 
-export async function isFollowingAddr( apiKey: API_KEY, addr: AddressStr ): Promise<boolean>
+export async function isFollowingAddr( ipAddress: AddressStr ): Promise<boolean>
 {
     const redis = await getRedisClient();
-    return await redis.sIsMember( `${ADDR_TO_API_SET_PREFIX}:${addr}`, apiKey );
+    return await redis.sIsMember( `${ADDR_TO_API_SET_PREFIX}:${ipAddress}`, PUBLIC_API_KEY );
 }
 
-export async function followAddr( addr: AddressStr, apiKey: API_KEY ): Promise<void>
+export async function followAddr( ipAddress: AddressStr ): Promise<void>
 {
     const redis = await getRedisClient();
-    await redis.sAdd( `${ADDR_TO_API_SET_PREFIX}:${addr}`, apiKey );
-    await redis.sAdd( `${API_TO_ADDR_SET_PREFIX}:${apiKey}`, addr );
+    await redis.sAdd( `${API_TO_ADDR_SET_PREFIX}:${ipAddress}`, PUBLIC_API_KEY );
+    await redis.sAdd( `${API_TO_ADDR_SET_PREFIX}:${PUBLIC_API_KEY}`, ipAddress );
 }
 
-export async function unfollowAddr( addr: AddressStr, apiKey: API_KEY ): Promise<void>
+export async function unfollowAddr( ipAddress: AddressStr ): Promise<void>
 {
     const redis = await getRedisClient();
     
-    let card =  await redis.sRem( `${ADDR_TO_API_SET_PREFIX}:${addr}`, apiKey );
-    if( card <= 0 )   redis .del( `${ADDR_TO_API_SET_PREFIX}:${addr}` );
+    let card = await redis.sRem( `${ADDR_TO_API_SET_PREFIX}:${ipAddress}`, PUBLIC_API_KEY );
+    if( card <= 0 ) redis.del( `${ADDR_TO_API_SET_PREFIX}:${ipAddress}` );
     
-    card =      await redis.sRem( `${API_TO_ADDR_SET_PREFIX}:${apiKey}`, addr );
-    if( card <= 0 )   redis .del( `${API_TO_ADDR_SET_PREFIX}:${apiKey}` );
+    card = await redis.sRem( `${API_TO_ADDR_SET_PREFIX}:${PUBLIC_API_KEY}`, ipAddress );
+    if( card <= 0 ) redis.del( `${API_TO_ADDR_SET_PREFIX}:${PUBLIC_API_KEY}` );
 }
